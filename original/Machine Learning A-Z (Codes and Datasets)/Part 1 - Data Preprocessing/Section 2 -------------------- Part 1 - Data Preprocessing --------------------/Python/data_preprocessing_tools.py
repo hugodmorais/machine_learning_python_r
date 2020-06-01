9436c6ddd1304e3458012d940fe247d75084c6da
hugodmorais <hugodanielmorais@gmail.com>
2020-06-01 22:13:05 +0100

    import libraries and dataset

--- original/Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/data_preprocessing_tools.py
+++ original/Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/data_preprocessing_tools.py
@@ -25,25 +25,25 @@ from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
-print(X)
+# print(X)
 # Encoding the Dependent Variable
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
 y = le.fit_transform(y)
-print(y)
+# print(y)
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
-print(X_train)
-print(X_test)
-print(y_train)
-print(y_test)
+# print(X_train)
+# print(X_test)
+# print(y_train)
+# print(y_test)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
 X_test[:, 3:] = sc.transform(X_test[:, 3:])
-print(X_train)
-print(X_test)
+# print(X_train)
+# print(X_test)
